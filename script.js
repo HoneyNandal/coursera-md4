7ae110ba780984e099d8ee9b50cc48ea5c86406f
Honey Nandal <honeynandal81@gmail.com>
2022-01-08 00:22:19 +0530

    coursera-md4

---
+++ script.js
@@ -0,0 +1,17 @@
+(function () {
+
+var names = ["Honey", "Juju", "Kiara", "Tyler", "Annie", "Ariella", "Lara", "Jack", "Julia", "Garry"];
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    console.log("Good Bye" + " " +(names[i]));
+  } else {
+    console.log("Hello" + " " + (names[i]));
+  }
+}
+
+})
+
+();
